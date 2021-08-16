93869dc6cc02fd4d2e266a27a5e6455d3e356f5a
aelgmal7 <aelgmal7@gmail.com>
2021-08-16 10:46:35 +0200

    q

--- src/main/java/com/example/onTime/Employees/EmployeeRepository.java
+++ src/main/java/com/example/onTime/Employees/EmployeeRepository.java
@@ -2,5 +2,5 @@ package com.example.onTime.Employees;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+public interface EmployeeRepository extends JpaRepository<Employee,Long>  {
 }
